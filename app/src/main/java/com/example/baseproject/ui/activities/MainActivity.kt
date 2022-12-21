3024fe546d2c3668cc4c3913bd83db030813aa03
ndhung1 <ndhung>
2022-12-21 17:41:01 +0700

    bottom nav

--- app/src/main/java/com/example/baseproject/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/baseproject/ui/activities/MainActivity.kt
@@ -6,10 +6,15 @@ import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.baseproject.R
+import com.example.baseproject.databinding.ActivityMainBinding
 import com.example.baseproject.ui.dialog.ModalBottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -17,16 +22,17 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val activityViewModel by viewModels<MainActivityViewModel>()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val content: View = findViewById(android.R.id.content)
             content.viewTreeObserver.addOnPreDrawListener(
-                object : ViewTreeObserver.OnPreDrawListener{
+                object : ViewTreeObserver.OnPreDrawListener {
                     override fun onPreDraw(): Boolean {
-                        return if(activityViewModel.splashUiState.value.isSplashScreenEnded){
+                        return if (activityViewModel.splashUiState.value.isSplashScreenEnded) {
                             content.viewTreeObserver.removeOnPreDrawListener(this)
                             //Nothing to do here, let the virtual SplashFragment handle the rest
                             true
@@ -39,14 +45,30 @@ class MainActivity : AppCompatActivity() {
         } else {
             //do nothing, so that SplashFragment keep showing on Screen
         }
+        initNavigation()
         observeDataChange()
     }
 
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+        val listOfNoBottomNavFragment = setOf<Int>(R.id.splashFragment, R.id.introFragment)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (listOfNoBottomNavFragment.contains(destination.id)) {
+                binding.bottomNav.visibility = View.GONE
+            } else {
+                binding.bottomNav.visibility = View.VISIBLE
+            }
+        }
+    }
+
     private fun observeDataChange() {
-        lifecycleScope.launch{
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                activityViewModel.bottomSheetUiState.collect{
-                    if(it.isShow){
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                activityViewModel.bottomSheetUiState.collect {
+                    if (it.isShow) {
                         val bottomSheetDialogFragment = ModalBottomSheetDialogFragment()
                         bottomSheetDialogFragment.show(supportFragmentManager, "TAG")
                     }
